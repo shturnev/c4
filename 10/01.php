ae138955134f22a5ccd16f22d94efcd224fc3d77
shturnev <sht_job@ukr.net>
2017-03-31 19:27:15 +0300

    +1

--- 10/01.php
+++ 10/01.php
@@ -4,5 +4,18 @@ require "DB.php";
 $DB = new DB();
 
 
+try{
+
+    $arr = ["nickname" => 'вавававава'];
+    $resUpd = $DB->update('test', $arr, 'ID = 22');
+
+
+    $resRow = $DB->get_rowS("SELECT * FROM test ORDER BY ID DESC");
+    $resRow2 = array_reverse($resRow);
+
+}
+catch(Exception $e){
+    $dsdsdsd = $e;
+}
 
 $DSDSD = 1;
