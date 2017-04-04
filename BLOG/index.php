e98cd54a726b56669ab557600ecec9e1fcd25e5f
stsvstsv <stsvstsv@mail.ru>
2017-04-05 00:13:11 +0300

    добавлен файл

--- BLOG/index.php
+++ BLOG/index.php
@@ -5,3 +5,43 @@
  * Date: 04.04.2017
  * Time: 20:35
  */
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Блог Сергея</title>
+</head>
+<body>
+<div id="belt">
+    <header>
+        <div class="logo">Цыбульник Сергей</div>
+        <a href="#">Авторизация</a>
+        <a href="#">Регистрация</a>
+        <a href="#">Выход</a>
+    </header>
+    <section class="content">
+        <a href="#" class="add">добавить</a>
+        <div class="article">
+            <h1>Заглавие</h1>
+            <div class="text">
+                Lorem ipsum dolor sit amet.
+            </div>
+            <a href="#" class="edit">редактировать</a>
+            <a href="#" class="delete">удалить</a>
+        </div>
+        <div class="article">
+            <h1>Заглавие</h1>
+            <div class="text">
+                Lorem ipsum dolor sit amet.
+            </div>
+            <a href="#" class="edit">редактировать</a>
+            <a href="#" class="delete">удалить</a>
+        </div>
+    </section>
+</div>
+</body>
+</html>
