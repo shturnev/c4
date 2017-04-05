0d4b8d0ea0dc175631e1766e68dcfcb3d25c1964
stsvstsv <stsvstsv@mail.ru>
2017-04-05 19:48:22 +0300

    gjcktlyttt

--- BLOG/index.php
+++ BLOG/index.php
@@ -25,23 +25,17 @@
     <section class="content">
         <a href="" class="add">добавить</a>
 
-        <div class="article">
-            <a href="blog_in.php" class="title"><h1>Заглавие</h1></a>
-            <div class="text">
-                Lorem ipsum dolor sit amet.
+        <?php for($i=0; $i<10; $i++) {
+            ?>
+            <div class="article">
+                <a href="blog_in.php" class="title"><h1>Заглавие</h1></a>
+                <div class="text">
+                    Lorem ipsum dolor sit amet.
+                </div>
+                <a href="#" class="edit">редактировать</a>
+                <a href="#" class="delete">удалить</a>
             </div>
-            <a href="#" class="edit">редактировать</a>
-            <a href="#" class="delete">удалить</a>
-        </div>
-
-        <div class="article">
-            <a href="" class="title"><h1>Заглавие</h1></a>
-            <div class="text">
-                Lorem ipsum dolor sit amet.
-            </div>
-            <a href="#" class="edit">редактировать</a>
-            <a href="#" class="delete">удалить</a>
-        </div>
+            <?}?>
 
     </section>
 </div>
