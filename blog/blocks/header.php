b410ef3f80c746c2787e7d6e92869e4ffca4082f
Helena0610 <luckybigdog@mail.ru>
2017-04-05 19:47:41 +0300

    Привести внешний вид в порядок

---
+++ blog/blocks/header.php
@@ -0,0 +1,15 @@
+<nav>
+    <ul class="nav">
+        <li>
+            <a href="">Выход</a>
+        </li>
+
+        <li>
+            <a href="blog_login.php">Авторизация</a>
+        </li>
+        <li>
+            <a href="blog_register.php">Регистрация</a>
+        </li>
+    </ul>
+</nav>
+
--- blog/index.php
+++ blog/index.php
@@ -1,4 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>blog_nikolay</title>
+    <link rel="stylesheet" href="css/style_start.css">
+</head>
+<body>
+<div class="main">
+    <?php require 'blocks/header.php' ?>
+    <div class="button">
+        <a class="button" href="">Добавить</a>
+    </div>
 
-?>
 
+    <ul>
+        <? for ($i = 0; $i < 40; $i++){ ?>
+        <li>
+            <a href="blog_in.php">Lorem ipsum.</a>
+            <div class="descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur corporis earum
+                inventore laudantium pariatur qui totam vero? Consectetur dolorem eius illo libero minima nesciunt nisi
+                odio, officia perferendis repellendus similique?
+            </div>
+            <div class="adm_btns">
+                <a href="#">Редактировать</a>
+                <a href="#">Удалить</a>
+            </div>
+        </li>
+        <? } ?>
+    </ul>
+
+
+</div>
+
+
+
+</body>
+</html>
