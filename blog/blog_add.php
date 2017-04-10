da922af7b1fb927d303b29dde66a0aec45d5e8ed
2mykola <2niklev@gmail.com>
2017-04-10 19:25:34 +0300

    +

--- blog/blog_add.php
+++ blog/blog_add.php
@@ -1,4 +1,39 @@
-<?php
 
-?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8"/>
+    <title></title>
+    <link rel="shortcut icon" href=""/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="all" href=""/>
+    <link rel="stylesheet" href="css/style_start.css">
 
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+
+</head>
+
+<body>
+
+<div class="main">
+    <?php require 'blocks/header.php' ?>
+
+    <div class="form">
+        <form action="" method="post" enctype="multipart/form-data" name="myForm" target="_self">
+            <input type="text" name="email" value="" placeholder="email"/><br><br>
+            <input type="password" name="pass" value="" placeholder="password"/><br><br>
+
+            <input name="submit" type="submit" value="готово"/>
+        </form>
+
+    </div>
+
+
+
+</div>
+
+<script type="text/javascript" src=""></script>
+</body>
+</html>
